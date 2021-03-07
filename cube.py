af990ae418f12cc4e0ac56163f4d8bab2a66e553
mitselek <mihkel.putrinsh@gmail.com>
2021-03-07 16:17:41 +0200

    we can write words to grid

--- cube.py
+++ cube.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from random import randint, shuffle, sample, choice
+import json
 
 DIRECTIONS = {
-    "xy": {"dx": +1, "dy": -1, "dz":  0},
-    "xz": {"dx": +1, "dy":  0, "dz": -1},
-    "yz": {"dx":  0, "dy": +1, "dz": -1},
-    "yx": {"dx": -1, "dy": +1, "dz":  0},
-    "zx": {"dx": -1, "dy":  0, "dz": +1},
-    "zy": {"dx":  0, "dy": -1, "dz": +1}
+    "xy": {"dx": +1, "dy": -1, "dz": 0},
+    "xz": {"dx": +1, "dy": 0, "dz": -1},
+    "yz": {"dx": 0, "dy": +1, "dz": -1},
+    "yx": {"dx": -1, "dy": +1, "dz": 0},
+    "zx": {"dx": -1, "dy": 0, "dz": +1},
+    "zy": {"dx": 0, "dy": -1, "dz": +1}
 }
+
 
 class Cube:
     def __init__(self, x, y, z):
@@ -25,41 +27,48 @@ class Cube:
     @property
     def coords(self):
         return self.__coords
+
     @property
     def letter(self):
         return self.__letter
+
     @property
     def links(self):
         return self.__links
+
     @property
     def abs_min(self):
         return min(abs(self.__coords['x']), abs(self.__coords['y']), abs(self.__coords['z']))
+
     @property
     def abs_max(self):
         return max(abs(self.__coords['x']), abs(self.__coords['y']), abs(self.__coords['z']))
+
     @property
     def numlinks(self):
         return len(self.__links)
+
     @property
     def x(self):
         return self.__coords['x']
+
     @property
     def y(self):
         return self.__coords['y']
+
     @property
     def z(self):
         return self.__coords['z']
 
-
     def max_word_length(self, radius):
-        return (radius + 1) + self.abs_max - self.abs_min # 
+        return (radius + 1) + self.abs_max - self.abs_min  #
 
     def engrave(self, dir, word):
-        print('engraving', word, 'to', dir)
+        # print('engraving', word, 'to', dir, self)
         self.__letter = word[0]
         if len(word) > 1:
-            self.links[dir].engrave(dir, word[1:])
-
+            return self.links[dir].engrave(dir, word[1:])
+        return True
 
     def link(self, dir_label, other_cube):
         self.links.setdefault(dir_label, other_cube)
@@ -97,7 +106,7 @@ class Cube:
             if next_cube.numlinks == 1:
                 next_cube.grow(cubes, radius)
 
-        return 
+        return
 
 
 class Tiling:
@@ -106,36 +115,59 @@ class Tiling:
         self.cubes = {}
         centerCube = self.cubes.setdefault((0, 0, 0), Cube(0, 0, 0))
         centerCube.grow(self.cubes, self.radius)
-    
+        self.words = []
+
     def __str__(self):
-        coords = [(coord, self.cubes[coord].letter) for coord in self.cubes]
-        return str(coords)
-    
+        coords = [
+            (
+                coord,
+                self.cubes[coord].letter,
+                # [
+                #     link
+                #     for link in self.cubes[coord].links.keys()
+                # ]
+            )
+            for coord in self.cubes
+        ]
+        return json.dumps(coords)
+
+    @property
+    def tiles(self):
+        return {
+            coord: self.cubes[coord].letter
+            for coord in self.cubes
+        }
+
     def max_word_length(self, cube, direction=None):
         if direction:
             # print(cube, direction)
-            max_x = 2*self.radius+1 if direction['dx'] == 0 else abs(direction['dx'] * self.radius - cube.x) + 1
-            max_y = 2*self.radius+1 if direction['dy'] == 0 else abs(direction['dy'] * self.radius - cube.y) + 1
-            max_z = 2*self.radius+1 if direction['dz'] == 0 else abs(direction['dz'] * self.radius - cube.z) + 1
-            return min(max_x, max_y, max_z)
+            max_x = 2 * self.radius + 1 if direction['dx'] == 0 else abs(direction['dx'] * self.radius - cube.x) + 1
+            max_y = 2 * self.radius + 1 if direction['dy'] == 0 else abs(direction['dy'] * self.radius - cube.y) + 1
+            max_z = 2 * self.radius + 1 if direction['dz'] == 0 else abs(direction['dz'] * self.radius - cube.z) + 1
+            maxlen = min(max_x, max_y, max_z)
+            # print('maxlen', maxlen)
+            return maxlen
         else:
-            return (self.radius + 1) + cube.abs_max - cube.abs_min # 
+            return (self.radius + 1) + cube.abs_max - cube.abs_min  #
 
     def engrave(self, word, hint=None):
-        print('engraving "', word, '"')
         cubes = list(self.cubes.values())
         shuffle(cubes)
         for cube in cubes:
-            print('\nTry', cube)
+            # print('\nTry', cube)
             if self.max_word_length(cube) < len(word):
                 continue
             directions = list(DIRECTIONS.items())
             shuffle(directions)
             for dir_label, direction in directions:
+                # print(dir_label)
                 max_word_length = self.max_word_length(cube, direction)
-                if max_word_length < len(word):
+                if max_word_length < len(word[0]):
                     continue
-                cube.engrave(dir_label, word)
+                if cube.engrave(dir_label, word[0]):
+                    print('Engraved', word[0], 'to', cube.coords, '->', dir_label)
+                    self.store(word)
                 return
 
-
+    def store(self, word):
+        self.words.append(word)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
+from random import shuffle
 
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import cm
@@ -12,35 +14,44 @@ from cube import Tiling
 MINWORDS, MAXTRY, WORDLENGTH, RADIUS, RETRY_COUNT = 10, 100, 6, 6, 1000
 
 BOOKLET_PAGES = 2
+PDF_GRID_CENTER = 21 / 2 * cm
 PDF_FONT_SIZE = 16 * 16 / RADIUS
 PDF_LEFT = (21 - (RADIUS - 0.5) * PDF_FONT_SIZE / 16) / 2 * cm
 PDF_TOP = 29.7 * cm - PDF_LEFT
 
-PLACEHOLDER = '_'
+PLACEHOLDER = '-'
 DIRECTIONS = ((0,-1,1), (1,-1,0), (1,0,-1), (0,1,-1), (-1,1,0), (-1,0,1))
 E_WORD_TOO_LONG = 'Nii pikk sõna ei mahu ära'
 
 WORDPOOL_INGREDIENTS = [
-    { 'size': 100, 're': '[KPTGBD][AEIOUÕÜÄÖ][KPTGBD][AEIOUÕÜÄÖ][KPTGBD]' }
+    {'size': 22, 're': '[KPTGBD][AEIOUÕÜÄÖ][KPTGBD][AEIOUÕÜÄÖ][KPTGBD]'},
+    {'size': 22, 're': '[AEIOUÕÜÄÖ][KPTGBD][AEIOUÕÜÄÖ][AEIOUÕÜÄÖ]'}
 ]
-RADIUS = 3
+RADIUS = 2
 MIN_WORD_LENGTH = min(4, RADIUS + 1)
 MAX_WORD_LENGTH = 2 * RADIUS + 1
 FIRST_WORD_LENGTH = 2 * RADIUS
 
 
 def new_words():
-    words = ''
+    words = []
     for wp in WORDPOOL_INGREDIENTS:
         shell_str = "cat et/4.txt | grep '^.\{" + str(MIN_WORD_LENGTH) + "," + str(MAX_WORD_LENGTH) + "\}$' | grep '" + wp['re'] + "' | sort -R | head -" + str(wp['size'])
         print(shell_str)
         stream = os.popen(shell_str)
+        wordchain = ''
         for word in stream.readlines():
-            print(word)
-            words += word
-        words = [(word, word) for word in words.rstrip("\n").split('\n')]
-        print(words)
-tiling = Tiling(2)
-tiling.engrave('bars')
+            wordchain += word
+        words.extend([(word.rstrip("\n"), word.rstrip("\n")) for word in wordchain.rstrip("\n").split('\n')])
+    shuffle(words)
+    return words
+
+tiling = Tiling(RADIUS)
 print(tiling)
-new_words()
+words = new_words()
+for word in words:
+    tiling.engrave(word)
+
+tiles = tiling.tiles
+print(tiles)
+# print(tiling.words)
